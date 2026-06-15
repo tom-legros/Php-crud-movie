effa1cb087e94af13bba1717d5b26e93b5b41789
Théo Bordier <theobordier@hotmail.com>
2026-06-15 14:29:17 +0200

    création du fichier Entity & de la classe people, ajout des accesseurs et des variables.

---
+++ src/Entity/people.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+use Database\MyPdo;
+
+class Artist
+{
+    private int $avatarId;
+    private string $name;
+    private \DateTime $birthday;
+    private \DateTime $deathday;
+    private string $biography;
+    private string $placeOfBirth;
+
+    private function __construct() {}
+
+    public function getAvatarId(): int
+    {
+        return $this->avatarId;
+    }
+
+    public function setAvatarId(int $avatarId): void
+    {
+        $this->avatarId = $avatarId;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getBirthday(): \DateTime
+    {
+        return $this->birthday;
+    }
+
+    public function setBirthday(\DateTime $birthday): void
+    {
+        $this->birthday = $birthday;
+    }
+
+    public function getDeathday(): \DateTime
+    {
+        return $this->deathday;
+    }
+
+    public function setDeathday(\DateTime $deathday): void
+    {
+        $this->deathday = $deathday;
+    }
+
+    public function getPlaceOfBirth(): string
+    {
+        return $this->placeOfBirth;
+    }
+
+    public function setPlaceOfBirth(string $placeOfBirth): void
+    {
+        $this->placeOfBirth = $placeOfBirth;
+    }
+
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }
+
+    public function setBiography(string $biography): void
+    {
+        $this->biography = $biography;
+    }
+
+
+}
+
