a2f92658786be64823473bd073901b86490048a5
Legros Tom <tom.legros@etudiant.univ-reims.fr>
2026-06-15 15:42:19 +0200

    Modification de la Page Movies

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -1,2 +1,28 @@
 <?php
 declare(strict_types=1);
+
+class Image {
+    private int $imageId;
+    private string $jpeg;
+
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+    public function setJpeg(string $jpeg): void
+    {
+        $this->jpeg = $jpeg;
+    }
+
+    public function getImageId(): int
+    {
+        return $this->imageId;
+    }
+
+    public function setImageId(int $imageId): void
+    {
+        $this->imageId = $imageId;
+    }
+
+}
