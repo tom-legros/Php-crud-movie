7a46f4784deabbe2762ed93683faef0f54a70c7c
Legros Tom <tom.legros@etudiant.univ-reims.fr>
2026-06-15 14:24:03 +0200

    Ajout de Entity MovieCollection

---
+++ src/Entity/Collection/MovieCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Movie;
+
+class MovieCollection{
+    public function findAll(): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT posterId,originalLanguage,originalTitle,overwiew,releaseDate,runtime,tagline,title
+            FROM Movie
+            WHERE id IS NOT NULL
+            ORDER BY name ASC
+SQL
+        );
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, Movie::class);
+    }
+}
+
