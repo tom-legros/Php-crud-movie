2e8f95d7f71f7e034adb8b2a18c1fd6b08aceb1a
tomlgsdev <tom.legros@etudiant.univ-reims.fr>
2026-06-16 19:55:52 +0200

    Ajout de escapeString sur chaque getter de movie.php

--- public/movie.php
+++ public/movie.php
@@ -8,6 +8,7 @@ use Entity\Exception\EntityNotFoundException;
 use Entity\Movie;
 use Entity\Collection\PeopleCollection;
 use Html\WebPage;
+use Html\StringEscaper;
 
 if (!isset($_GET['movieId']) || !ctype_digit($_GET['movieId'])) {
     header('location: /');
@@ -34,18 +35,20 @@ $webPage->setTitle("Films - {$movie->getTitle()}");
 $webPage->appendCssUrl('/css/style.css');
 $webPage->appendCssUrl('/css/movie.css');
 
+$escaper = new class { use StringEscaper; };
+
 $content  = '<div class="header"><h1>'.$webPage->getTitle().'</h1></div>';
 $content .= '<div class="content">';
 $content .= '<div class="movie">';
-$content .= '<img class="movie-poster" src="data:image/jpeg;base64,' . $decodePoster . '" alt="' . $movie->getTitle() . '">';
+$content .= '<img class="movie-poster" src="data:image/jpeg;base64,'.$escaper->escapeString($decodePoster).'" alt="' . $escaper->escapeString($movie->getTitle()) . '">';
 $content .= '<div class="movie-info">';
 $content .= '<div class="movie-title-row">';
-$content .= '<span class="movie-title">'.$movie->getTitle().'</span>';
-$content .= '<span class="movie-date">'.$movie->getReleaseDate().'</span>';
+$content .= '<span class="movie-title">'.$escaper->escapeString($movie->getTitle()).'</span>';
+$content .= '<span class="movie-date">'.$escaper->escapeString($movie->getReleaseDate()).'</span>';
 $content .= '</div>';
-$content .= '<div class="movie-original-title">'.$movie->getOriginalTitle().'</div>';
-$content .= '<div class="movie-tagline">'.$movie->getTagline().'</div>';
-$content .= '<div class="movie-overview">'.$movie->getOverview().'</div>';
+$content .= '<div class="movie-original-title">'.$escaper->escapeString($movie->getOriginalTitle()).'</div>';
+$content .= '<div class="movie-tagline">'.$escaper->escapeString($movie->getTagline()).'</div>';
+$content .= '<div class="movie-overview">'.$escaper->escapeString($movie->getOverview()).'</div>';
 $content .= '</div>';
 $content .= '</div>';
 
@@ -54,16 +57,15 @@ foreach ($actors as $actor) {
     $actorAvatar = $actor->getAvatarById($actor->getAvatarId());
     $decodeAvatar = base64_encode($actorAvatar->getJpeg());
 
-    $content .= '<a class="cast-card" href="/actor.php?peopleId=' . $actor->getId() . '">';
-    $content .= '<img class="cast-avatar" src="data:image/jpeg;base64,' . $decodeAvatar . '" alt="' . $actor->getName() . '">';
+    $content .= '<a class="cast-card" href="/actor.php?peopleId=' . $escaper->escapeString((string) $actor->getId()) . '">';
+    $content .= '<img class="cast-avatar" src="data:image/jpeg;base64,' . $escaper->escapeString($decodeAvatar) . '" alt="' . $escaper->escapeString($actor->getName()) . '">';
     $content .= '<div class="cast-info">';
-    $content .= '<span class="cast-role">' . $actor->getRole() . '</span>';
-    $content .= '<span class="cast-name">' . $actor->getName() . '</span>';
+    $content .= '<span class="cast-role">'.$escaper->escapeString($actor->getRole()).'</span>';
+    $content .= '<span class="cast-name">'.$escaper->escapeString($actor->getName()).'</span>';
     $content .= '</div>';
     $content .= '</a>';
 }
 $content .= '</div>';
-
 $content .= '</div>';
 
 $webPage->appendContent($content);
