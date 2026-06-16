c590348cdd31689e421bfd45f44b15d82048fd23
Legros Tom <tom.legros@etudiant.univ-reims.fr>
2026-06-16 12:30:37 +0200

    Page movie Final sans les acteurs + CSS de movie

--- public/movie.php
+++ public/movie.php
@@ -6,12 +6,10 @@ namespace Html;
 
 use Entity\Exception\EntityNotFoundException;
 use Entity\Movie;
-use Entity\Collection\MovieCollection;
-use Html\AppWebPage;
+use Html\WebPage;
 
 if (!isset($_GET['movieId']) || !ctype_digit($_GET['movieId'])) {
     header('location: /');
-
     exit(302);
 }
 
@@ -24,14 +22,27 @@ try {
     exit;
 }
 
-$webPage = new AppWebPage();
-$webPage->setTitle("{$movie->getTitle()}");
-$webPage->appendContent("<h1>{$movie->getreleaseDate()}</h1>");
-$webPage->appendContent("<h1>{$movie->getOriginalTitle()}</h1>");
-$webPage->appendContent("<h1>{$movie->gettagline()}</h1>");
-$webPage->appendContent("<h1>{$movie->getOverview()}</h1>");
+$poster = $movie->getPosterById($movie->getPosterID());
+$decodePoster = base64_encode($poster->getJpeg());
 
+$webPage = new WebPage();
+$webPage->setTitle("Films - {$movie->getTitle()}");
+$webPage->appendCssUrl('/css/style.css');
+$webPage->appendCssUrl('/css/movie.css');
+
+$content  = '<div class="header"><h1>'.$webPage->getTitle().'</h1></div>';
+$content .= '<div class="content">';
+$content .= '<div class="movie">';
+$content .= '<img class="movie-poster"'.'src="data:image/jpeg;base64,' . $decodePoster . '"' .' alt="' . $movie->getTitle() . '">';
+$content .= '<div class="movie-info">';
+$content .= '<div class="movie-title-row">';
+$content .= '<span class="movie-title">'.$movie->getTitle() .'</span>';
+$content .= '<span class="movie-date">' .$movie->getReleaseDate().'</span>';
+$content .= '</div>';
+$content .= '<div class="movie-original-title">' .$movie->getOriginalTitle().'</div>';
+$content .= '<div class="movie-tagline">'.$movie->getTagline(). '</div>';
+$content .= '<div class="movie-overview">'.$movie->getOverview().'</div>';
+$content .= '</div>'.'</div>'.'</div>';
+
+$webPage->appendContent($content);
 echo $webPage->toHTML();
-
-
-
