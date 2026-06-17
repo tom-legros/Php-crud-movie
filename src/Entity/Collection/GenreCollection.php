dc585ca85124a3616066f2e8ee16c78d7942d959
Théo Bordier <theobordier@hotmail.com>
2026-06-17 10:07:10 +0200

    Création de findByGenre dans GenreCollection

--- src/Entity/Collection/GenreCollection.php
+++ src/Entity/Collection/GenreCollection.php
@@ -2,6 +2,8 @@
 
 namespace Entity\Collection;
 
+use Entity\Movie;
+use Database\MyPdo;
 use Entity\Genre;
 
 class GenreCollection
@@ -20,4 +22,23 @@ class GenreCollection
 
         return $stmt->fetchAll(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, Genre::class);
     }
+
+    public function findByGenre(int $genreId): array
+    {
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+        SELECT m.posterId, m.originalLanguage, m.originalTitle, m.overview,
+               m.releaseDate, m.runtime, m.tagline, m.title, m.id
+        FROM movie m
+        JOIN movie_genre mg ON mg.movieId = m.id
+        WHERE m.posterId IS NOT NULL
+        AND mg.genreId = :genreId
+        ORDER BY m.title ASC
+        SQL
+        );
+        $stmt->execute([':genreId' => $genreId]);
+
+        return $stmt->fetchAll(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, Movie::class);
+    }
 }
+
